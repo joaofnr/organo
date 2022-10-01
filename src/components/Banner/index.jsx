328d0ff5dbf234656d0469e2832f002e565b2eaf
João Nunes Rios <joao.rios@compasso.com.br>
2022-10-01 15:37:29 -0300

    Begun components

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+function Banner () {
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner principal" />
+        </header>
+    )
+}
+
+export default Banner
